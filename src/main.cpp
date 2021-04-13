c1ba9ad8da6a3b4f028d444cf9b22a22e78da482
Gregwar <g.passault@gmail.com>
2021-04-13 19:12:33 +0200

    pio

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,16 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <mbed.h>
+#include <USBSerial.h>
+#include "shell.h"
 
-#include "mbed.h"
-
-
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
+DigitalOut led(PB_1);
+DigitalOut usbDisconnect(PB_9);
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(LED1);
+  shell_init_usb();
+  usbDisconnect = 0;
 
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        ThisThread::sleep_for(100);
     }
 }
