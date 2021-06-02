5e352ff849a99c42478f750f8bb52b3eb76cd44f
Gurvan Le Direach <gurvan.le-direach@etu.u-bordeaux.fr>
2021-06-02 10:54:16 +0200

    implement fnct send and recieve for help to tests

--- src/com.cpp
+++ src/com.cpp
@@ -17,25 +17,45 @@ nRF24L01P_PTX PTX(CH1, COM_CE1, COM_CE1);
 nRF24L01P_PRX PRX_1(CH2_1, COM_CE2, COM_CE1);
 nRF24L01P_PRX PRX_2(CH2_2, COM_CE2, COM_CE1);
 
+int send(nRF24L01P_PTX PTX,char *buffer, int size) {
+    shell_println("Transmit");
+    int r = PTX.TransmitPacket(buffer, size);
+    shell_println(r);
+    return r;
+}
+
+int receive(nRF24L01P_PRX PRX, char *buffer) {
+    shell_println("Receive");
+    int size_packet = -1;
+    if (PRX.IsPacketReady()) {
+        size_packet = PRX.ReadPacket(buffer);
+    }
+    return size_packet;
+}
+
 SHELL_COMMAND(send, "emission") {
     //PTX.TransmitPacket(argv[0],sizeof(argv[0]));
   char c = 'a';
-  shell_println("Transmit");
-  int r = PTX.TransmitPacket(&c, 1);
-  shell_println(r);
+
+  int size_packet = send(PTX, argv[0], sizeof(argv[0]));
+  shell_println(size_packet);
 }
 
 SHELL_COMMAND(receive, "reception") {
-  if (PRX_1.IsPacketReady() && PRX_2.IsPacketReady()) {//définir comment faire ici
+  /*if (PRX_1.IsPacketReady() && PRX_2.IsPacketReady()) {//définir comment faire ici
     char buffer_1;
     int size_packet = PRX_1.ReadPacket(&buffer_1);
-    /*//test size_packet
+    if(size_packet > 1) shell_println("(com.cpp) error: ReadPacket -> buffer_1");
     char buffer_2;
     size_packet = PRX_2.ReadPacket(&buffer_2);
-    //test size_packet
+    if(size_packet > 1) shell_println("(com.cpp) error: ReadPacket -> buffer_2");
     if (buffer_1 & buffer_2) shell_println(buffer_1);
-   */ 
-  }
+  }*/
+  char buffer_1;
+  char buffer_2;
+  int size_packet_1 = receive(PRX_1, &buffer_1);
+  int size_packet_2 = receive(PRX_2, &buffer_2);
+  if(size_packet_1 != size_packet_2) shell_println("(com.cpp) error : recieve -> size of variables 'size_packet_X' is different");
   wait_us(1000);
 }
 
--- src/com.h
+++ src/com.h
@@ -1,2 +1,6 @@
 #pragma once
 void com_init();
+
+int send(nRF24L01P_PTX PTX,char *buffer, int size);
+
+int receive(nRF24L01P_PRX PRX, char *buffer);
