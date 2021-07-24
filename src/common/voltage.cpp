4e7c2b175f27d4828fcf2a7a54c8a15889033c5e
Etienne Schmitz <contact@etienne-schmitz.com>
2021-07-24 13:52:56 +0200

    chore - remove unused statement

--- src/common/voltage.cpp
+++ src/common/voltage.cpp
@@ -6,15 +6,12 @@
 #include "swo.h"
 
 static naelic::SWO swo;
-static bool bat_init = false;
 
 SHELL_PARAMETER_FLOAT(bat, "Battery 1", 0.0);
-// Timer millis;
 
 void voltage_init()
 {
     bat = 0;
-    bat_init = false;
 }
 
 void voltage_tick()
@@ -24,20 +21,9 @@ void voltage_tick()
     if ((Kernel::Clock::now().time_since_epoch().count() - lastSample) > 5)
     {
         lastSample = Kernel::Clock::now().time_since_epoch().count();
-
         float v1 = mux_sample(BAT_ADDR) * 3.3 / (float)(1 << 16);
-        v1 = v1 * (BAT_R1 + BAT_R2) / BAT_R2;
-
-        bat = v1;
-
-        swo.println((int) bat);
-
-        // if (bat_init) {
-        //     bat_init = false;
-        //     bat = v1;
-        // } else {
-        //     bat = bat*0.99 + v1*0.01;
-        // }
+        bat = v1 * (BAT_R1 + BAT_R2) / BAT_R2;
+        swo.println((int)bat);
     }
 }
 
